28738019549e67c075f8908a1da1e8e07dc9b071
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-27 01:27:38 +0200

    Fill pokemon-profile controller to get / display a pokemon profile

--- src/public/js/app/controllers/pokemon-profile.controller.js
+++ src/public/js/app/controllers/pokemon-profile.controller.js
@@ -12,9 +12,43 @@
 
     function PokemonProfileController($rootScope, $scope, pokeApiService) {
         var Pp = this;
+        Pp.isReady = false;
 
+        Pp.showPokemonProfile = function (id) {
+            Pp.isReady = false;
+            getPokemonProfile(id)
+                .then(function (pokemon) {
+                    Pp.pokemon = pokemon;
+                    Pp.isReady = true;
+                })
+        };
+
+        function getPokemonProfile (id) {
+            var pokemon;
+            return pokeApiService.getPokemonById(id)
+                .then(function (pkmn) {
+                    pokemon = pkmn;
+                    return pokemon.species.url;
+                })
+                .then(pokeApiService.getSpecieByURL)
+                .then(function (specie) {
+                    pokemon.description = specie.flavor_text_entries[0].flavor_text;
+                    return specie.evolution_chain.url;
+                })
+                .then(function (evChainURL) {
+                    return pokeApiService.getEvolutionChainByURL(evChainURL, pokemon.id);
+                })
+                .then(pokeApiService.getEvolutionsSprites)
+                .then(function (evolutions) {
+                    pokemon.evolutions = evolutions;
+                    return pokemon;
+                })
+                .catch(function (e) {
+                    console.log(e);
+                });
+        };
         $rootScope.$on("showPokemonProfile", function(e, id) {
-            console.log(id);
+            Pp.showPokemonProfile(id);
         });
     }
 }());
