4e5ecfa8ee680649313496e5a067aa8487946c1f
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-27 02:08:12 +0200

    Add the search/filter logic un Pl controller

--- src/public/js/app/controllers/pokemon-list.controller.js
+++ src/public/js/app/controllers/pokemon-list.controller.js
@@ -14,11 +14,17 @@
         var Pl = this;
 
         Pl.title = "Pokemon List";
+        Pl.allPokemons = [];
         Pl.pokemons = [];
+        Pl.isReady = false;
 
         Pl.showPokemonProfile = function (id) {
             $rootScope.$emit("showPokemonProfile", id);
         };
+
+        $rootScope.$on("filterPokemonList", function (e, string) {
+            filterPokemonList(string);
+        });
 
         init();
 
@@ -27,10 +33,22 @@
         }
 
         function getPokemonList () {
+            Pl.isReady = false;
             pokeApiService.getPokemonList()
                 .then(function (pokemons) {
+                    Pl.allPokemons = pokemons;
                     Pl.pokemons = pokemons;
+                    Pl.isReady = true;
                 });
         }
+
+        function filterPokemonList(string) {
+            var pokemons = Pl.allPokemons.filter(function (pokemon) {
+                if (pokemon.name.substr(0, string.length) === string || pokemon.id.toString().substr(0, string.length) === string) {
+                    return pokemon
+                }
+            });
+            Pl.pokemons = pokemons;
+        };
     }
 }());
