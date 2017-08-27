75bae3653fdd3786cfd1775cd64cac4c30ecbcde
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-27 19:47:34 +0200

    Fill the team controller

--- src/public/js/app/controllers/team.controller.js
+++ src/public/js/app/controllers/team.controller.js
@@ -8,10 +8,21 @@
         .module("wakemon")
         .controller("Team", TeamController);
 
-    TeamController.$inject = ["$rootScope", "teamService"];
+    TeamController.$inject = ["$rootScope", "teamService", "$state"];
 
-    function TeamController($rootScope, teamService) {
+    function TeamController($rootScope, teamService, $state) {
         var T = this;
-        console.log("Team controller");
+        T.pokemons = [];
+
+        T.removePokemonFromTeam = function (pokemon) {
+            teamService.removePokemonFromTeam(pokemon);
+            getTeam();
+        };
+
+        function getTeam() {
+            T.pokemons = teamService.getTeam();
+        }
+
+        getTeam();
     }
 }());
